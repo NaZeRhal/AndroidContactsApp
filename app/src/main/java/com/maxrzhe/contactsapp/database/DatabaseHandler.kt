23a7a1c36029c1f0b8530e01f4f920307c7ad1fb
NaZeRhal <zhanshi@yandex.ru>
2021-04-16 16:46:55 +0300

    fixing

--- app/src/main/java/com/maxrzhe/contactsapp/database/DatabaseHandler.kt
+++ app/src/main/java/com/maxrzhe/contactsapp/database/DatabaseHandler.kt
@@ -39,7 +39,7 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(
         onCreate(db)
     }
 
-    fun add(contact: Contact): Long {
+    fun add(contact: Contact.New): Long {
         val db = this.writableDatabase
         val contentValues = getContentValue(contact)
 
@@ -52,7 +52,7 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(
         return success
     }
 
-    fun update(contact: Contact): Int {
+    fun update(contact: Contact.Existing): Int {
         val db = this.writableDatabase
         val contentValues = getContentValue(contact)
 
@@ -66,7 +66,7 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(
         return success
     }
 
-    fun delete(contact: Contact): Int {
+    fun delete(contact: Contact.Existing): Int {
         val db = this.writableDatabase
 
         val success = db.delete(
@@ -78,8 +78,8 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(
         return success
     }
 
-    fun findAll(): List<Contact> {
-        var contacts: List<Contact> = emptyList()
+    fun findAll(): List<Contact.Existing> {
+        var contacts: List<Contact.Existing> = emptyList()
         val query = "SELECT * FROM $TABLE_CONTACTS"
 
         val db = this.readableDatabase
@@ -127,12 +127,12 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(
         return contentValues
     }
 
-    fun findById(id: Long): Contact {
+    fun findById(id: Long): Contact.Existing? {
         val query = "SELECT * FROM $TABLE_CONTACTS WHERE $KEY_ID=$id"
 
         val db = this.readableDatabase
         val cursor: Cursor?
-        var contact = Contact.New()
+        var contact: Contact.Existing? = null
 
         try {
             cursor = db.rawQuery(query, null)
@@ -141,7 +141,7 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(
                 with(raw) {
                     if (moveToFirst()) {
                         do {
-                            contact = Contact.New(
+                            contact = Contact.Existing(
                                 id = getLong(getColumnIndex(KEY_ID)),
                                 name = getString(getColumnIndex(KEY_NAME)),
                                 phone = getString(getColumnIndex(KEY_PHONE)),
--- app/src/main/java/com/maxrzhe/contactsapp/model/Contact.kt
+++ app/src/main/java/com/maxrzhe/contactsapp/model/Contact.kt
@@ -4,7 +4,6 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 sealed class Contact {
-    abstract val id: Long
     abstract val name: String
     abstract val email: String
     abstract val phone: String
@@ -12,7 +11,6 @@ sealed class Contact {
 
     @Parcelize
     data class New(
-        override val id: Long = 0,
         override val name: String = "",
         override val email: String = "",
         override val phone: String = "",
@@ -21,10 +19,14 @@ sealed class Contact {
 
     @Parcelize
     data class Existing(
-        override val id: Long,
+        val id: Long,
         override val name: String,
         override val email: String,
         override val phone: String,
         override val image: String
     ) : Contact(), Parcelable
+
+    companion object {
+        fun emptyContact(): Existing = Existing(0, "", "", "", "")
+    }
 }
