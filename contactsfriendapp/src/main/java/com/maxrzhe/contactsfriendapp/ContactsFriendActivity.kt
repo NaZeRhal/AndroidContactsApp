2e8b81dca741d5a20d6097c0ed67875aa6aa80ff
NaZeRhal <zhanshi@yandex.ru>
2021-04-23 09:21:30 +0300

    add new module

---
+++ contactsfriendapp/src/main/java/com/maxrzhe/contactsfriendapp/ContactsFriendActivity.kt
@@ -0,0 +1,11 @@
+package com.maxrzhe.contactsfriendapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsFriendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts_friend)
+    }
+}
