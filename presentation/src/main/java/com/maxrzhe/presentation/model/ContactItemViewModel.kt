4ccdc67d49971f757d8e3ba6b35312060c916a47
NaZeRhal <zhanshi@yandex.ru>
2021-05-24 15:28:00 +0300

    change click handling in ContactItemViewModel

--- presentation/src/main/java/com/maxrzhe/presentation/model/ContactItemViewModel.kt
+++ presentation/src/main/java/com/maxrzhe/presentation/model/ContactItemViewModel.kt
@@ -1,16 +1,19 @@
 package com.maxrzhe.presentation.model
 
-import android.view.View
 import com.example.data_api.model.Contact
 import com.maxrzhe.presentation.R
+
+typealias Clicker = () -> Unit
 
 data class ContactItemViewModel(
     val contact: Contact,
     val onItemClick: (item: ContactItemViewModel) -> Unit,
-) : BaseItemViewModel(), View.OnClickListener {
+) : BaseItemViewModel() {
 
     override val id: Long get() = this.contact.hashCode().toLong()
     override val layoutId: Int get() = R.layout.item_contact
+
+    val clicker: Clicker = { onItemClick(this) }
 
     override fun isTheSameItem(baseItemViewModel: BaseItemViewModel): Boolean {
         return if (baseItemViewModel is ContactItemViewModel) {
@@ -23,8 +26,5 @@ data class ContactItemViewModel(
             this.contact == baseItemViewModel.contact
         } else false
     }
-
-    override fun onClick(v: View?) {
-        onItemClick(this)
-    }
 }
+
